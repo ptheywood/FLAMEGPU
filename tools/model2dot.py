40391c80e513e3cc3172cff079b9f266998fe468
Peter Heywood <peethwd@gmail.com>
2019-06-18 18:55:34 +0100

    Intiail work on much better dotgenerator

--- tools/model2dot.py
+++ tools/model2dot.py
@@ -1,134 +1,64 @@
 #!/usr/bin/python3
-# $Id$
-#
-# Authors: Mozhgan Kabiri Chimeh, Paul Richmond (based on Alcione de Paiva Oliveira existing version)
-#
-# This program is an extended/modified version written previously by Alcione de Paiva Oliveira.
-# Any questions, please contact m.kabiri-chimeh@sheffiled.ac.uk or p.richmond@sheffield.ac.uk
-#
-# Date : July 2017
-#
-# Description:
-# Create a dot direct graph from the Flame GPU model file.
-# By default the nodes are renamed to avoid direct cycles, so improving readability.
-#
-# Note: Feel free to remove "splines=ortho;", then you will have curved lines
-#---------------------------------------------------------------------
-#
-# input arguments
-# Example python3 model2dot.py -i XMLModelFile.xml -o out.gdot
 
-# To convert the dot graph to png: dot -Tpng out.gdot -o out.png
+# Generate dot file graph for the specified model.
+# Uses the graphviz package
+# Inspired by previous version (MKC + AP)
+# Redeveloped by PTH
 
-# @todo - use the graphviz package.
+# @todo - better exceptions.
+# @todo - remove UISS specialisation to merge back.
 
 import argparse
 import sys
+import os
 
-from xml.dom.minidom import parse
-import xml.dom.minidom
+import xml.etree.ElementTree as ET
 
 import graphviz
 
-#---------------------------------------------------------------------
-#    Get node in text format
-#---------------------------------------------------------------------
-def getNodeText(node):
-  nodelist = node.childNodes
-  result = []
-  for node in nodelist:
-    if node.nodeType == node.TEXT_NODE:
-      result.append(node.data)
-      return ''.join(result)
+# Dictionary of the expected XML namespaces, making search easier
+NAMESPACES = {
+  "xmml": "http://www.dcs.shef.ac.uk/~paul/XMML",
+  "gpu":  "http://www.dcs.shef.ac.uk/~paul/XMMLGPU"
+}
 
-#---------------------------------------------------------------------
-#    Get condition in text format
-#---------------------------------------------------------------------
-def getCondition(node):
-  globalCond = False
-  condT = (node.getElementsByTagName('condition'))
-  if (not condT):
-    condT = (node.getElementsByTagName("gpu:globalCondition"))
-    if (not condT):
-      return ""
-    else:
-      globalCond = True
-      lhsNode = node.getElementsByTagName('lhs')[0].getElementsByTagName('agentVariable')
-    if (lhsNode):
-      lhs = getNodeText(lhsNode[0])
-    else:
-      lhs = getNodeText(node.getElementsByTagName('lhs')[0].getElementsByTagName('value')[0])
+def verbose_log(args, msg):
+  if args.verbose:
+    print("Log: " + msg)
 
-      rhsNode = node.getElementsByTagName('rhs')[0].getElementsByTagName('agentVariable')
-      if (rhsNode):
-        rhs = getNodeText(rhsNode[0])
-      else:
-        rhs = getNodeText(node.getElementsByTagName('rhs')[0].getElementsByTagName('value')[0])
-
-        operator = getNodeText(node.getElementsByTagName('operator')[0])
-        if (operator == "&lt;"):
-          operator = "<"
-        elif (operator == "&le;"):
-          operator = "<="
-        elif (operator == "&gt;"):
-          operator = ">"
-        elif (operator == "&ge;"):
-          operator = ">="
-
-          if (globalCond):
-            return "[ label =\"global :"+ lhs+operator+rhs+"\"]"
-            return "[ label =\""+ lhs+operator+rhs+"\"]"
-
-
-#---------------------------------------------------------------------
-#    Rename states ocurring more than once
-#---------------------------------------------------------------------
-def renameStates(agent):
-  tam = len(funcOrder)
-  for i in range(tam):
-    if(previousState[i].find(agent+":")==0 and previousState[i]==afterState[i]):
-     afterState[i] = afterState[i]+"\'"
-     j = i+ 1
-    while (j < tam):
-      if (previousState[i] == previousState[j]):
-        previousState[j] = previousState[j]+"\'"
-        if (previousState[i] == afterState[j]):
-          afterState[j] = afterState[j]+"\'"
-          if (afterState[i] == afterState[j]):
-            afterState[j] = afterState[j]+"\'"
-            j = j+ 1
-
-def main():
-
-  # Declare variables with essentially global scope 
-  funcOrder =[]
-  previousState =[]
-  afterState=[]
-  conditions=[]
-  cycle = False
-  genFunc = False
-  entrada = ""
-  saida = ""
-  mes = []
-  primeiro = True
-  clusterNumber = 0
-
+def parse_arguments():
   # Get arguments using argparse
   parser = argparse.ArgumentParser(
     description="Generate a dot file from an xml model file."
-    )
+  )
   parser.add_argument(
-    "-i",
-    "--input",
+    "xmlmodelfile",
     type=str,
     help="Path to xml model file"
-    )
+  )
   parser.add_argument(
     "-o",
     "--output",
     type=str,
     help="Path to output location"
+  )
+  parser.add_argument(
+    "--render",
+    action="store_true",
+    help="Render the graphviz dotfile"
     )
+  parser.add_argument(
+    "-f",
+    "--force",
+    action="store_true",
+    help="Overwrite output files."
+    )
+  parser.add_argument(
+    "--verbose",
+    action="store_true",
+    help="Verbose output"
+    )
+  # This seems evil.
   parser.add_argument(
     "-c",
     "--cycles",
@@ -137,249 +67,193 @@ def main():
     )
 
   args = parser.parse_args()
-  print(args)
+  return args
 
-  entrada = args.input
-  saida = args.output
-  cycle = args.cycles
+def validate_arguments(args):
+  verbose_log(args, "Validating Arguments")
+
+  # Validate the path exists.
+  if os.path.exists(args.xmlmodelfile):
+    if not os.path.isfile(args.xmlmodelfile):
+      raise ValueError("xmlmodelfile {:} is not a file.".format(args.xmlmodelfile))
+  else:
+    raise ValueError("xmlmodelfile {:} does not exist.".format(args.xmlmodelfile))
+
+  # Validate the (optional) output file. 
+  if args.output is not None:
+    if os.path.exists(args.output):
+      if os.path.isfile(args.output) and not args.force:
+        raise ValueError("Output path {:} exists. Use --force to overwrite.".format(args.output))
+      elif os.path.isdir(args.output):
+        raise ValueError("Output path {:} is a directory.".format(args.output))
+
+
+def load_xmlmodelfile(args):
+  verbose_log(args, "Loading XML Model File")
+
+  # Attempt to parse the file. 
+  tree = ET.parse(args.xmlmodelfile)
+
+  return tree
+
+
+def get_model_name(args, xmlroot):
+  namexml = xmlroot.find('xmml:name', NAMESPACES)
+  return namexml.text
+
+def get_init_functions(args, xmlroot):
+  data = []
+  for x in xmlroot.findall('gpu:environment/gpu:initFunctions/gpu:initFunction/gpu:name', NAMESPACES):
+    data.append(x.text)
+  return data
+
+
+def get_step_functions(args, xmlroot):
+  data = []
+  for x in xmlroot.findall('gpu:environment/gpu:stepFunctions/gpu:stepFunction/gpu:name', NAMESPACES):
+    data.append(x.text)
+  return data
+
+def get_exit_functions(args, xmlroot):
+  data = []
+  for x in xmlroot.findall('gpu:environment/gpu:exitFunctions/gpu:exitFunction/gpu:name', NAMESPACES):
+    data.append(x.text)
+  return data
+
+def get_host_layer_functions(args, xmlroot):
+  data = []
+  for x in xmlroot.findall('gpu:environment/gpu:hostLayerFunctions/gpu:hostLayerFunction/gpu:name', NAMESPACES):
+    data.append(x.text)
+  return data
+
+def get_agent_names(args, xmlroot):
+  data = []
+  for x in xmlroot.findall('xmml:xagents/gpu:xagent/xmml:name', NAMESPACES):
+    data.append(x.text)
+  return data
+
+  # data = []
+  # xagents = xmlroot.find('xmml:xagents', NAMESPACES)
+  # if xagents is not None:
+  #   for xagent in xagents.findall('gpu:xagent', NAMESPACES):
+  #     name = xagent.find('xmml:name', NAMESPACES)
+  #     if name is not None:
+  #       data.append(name.text)
+  # return data
+
+def get_agent_functions(args, xmlroot):
+  data = {}
+  for xagent in xmlroot.findall('xmml:xagents/gpu:xagent', NAMESPACES):
+    xagent_name = xagent.find('xmml:name', NAMESPACES)
+    for function in xagent.findall('xmml:functions/gpu:function', NAMESPACES):
+      function_name = function.find('xmml:name', NAMESPACES)
+      if function_name is not None and xagent_name is not None:
+        if xagent_name.text not in data:
+          data[xagent_name.text] = {}
+        data[xagent_name.text][function_name.text] = {}
+  return data
+
+def get_message_names(args, xmlroot):
+  pass
+
+def get_agent_states(args, xmlroot):
+  pass
+
+
+def get_function_layers(args, xmlroot):
+  data = []
+  for layer in xmlroot.findall('xmml:layers/xmml:layer', NAMESPACES):
+    row = []
+    for layerFunctionName in layer.findall('gpu:layerFunction/xmml:name', NAMESPACES):
+      row.append(layerFunctionName.text)
+    data.append(row)
+  return data
+
+
+
+
+def generate_graphviz(args, xml):
+  verbose_log(args, "Generating Graphviz Data")
+  
+  
+  # Get the root element from the XML
+  xmlroot = xml.getroot()
+
+  model_name = get_model_name(args, xmlroot)
+  # print("Model: " + model_name)
+
+
+  # Get a bunch of data from the xml file. 
+  init_functions = get_init_functions(args, xmlroot)
+  step_functions = get_step_functions(args, xmlroot)
+  exit_functions = get_exit_functions(args, xmlroot)
+  host_layer_functions = get_host_layer_functions(args, xmlroot)
+  agent_names = get_agent_names(args, xmlroot)
+  agent_functions = get_agent_functions(args, xmlroot)
+  function_layers = get_function_layers(args, xmlroot)
+  
+  # print(init_functions)
+  # print(step_functions)
+  # print(exit_functions)
   
 
-  # Open XML document using minidom parser
-  DOMTree = xml.dom.minidom.parse(entrada)
-  collection = DOMTree.documentElement
+
+  # Create the digraph
+  dot = graphviz.Digraph(name=model_name, comment="FLAME GPU State Diagram")
 
 
-  dot = graphviz.Digraph(name="model", comment="FLAME GPU State Diagram")
+  # Add some global settings.
+  # file.write("  newrank=true;\ncompound=true; \n splines=ortho;\n")
+  # file.write("  START [style=invisible];\n");
+  # file.write("  MID [style=invisible];\n");
+  # file.write("  END [style=invisible];\n");
 
 
-
-
-
-  file = open(saida, "w")
-  file.write("digraph model {\n")
-
-  ####################
-
-  init_flag = False
-  step_flag = False
-  exit_flag = False
-
-  file.write("  newrank=true;\ncompound=true; \n splines=ortho;\n")
-
-  file.write("  START [style=invisible];\n");
-  file.write("  MID [style=invisible];\n");
-  file.write("  END [style=invisible];\n");
-
-  initFunc = collection.getElementsByTagName("gpu:initFunction")
-  stepFunc = collection.getElementsByTagName("gpu:stepFunction")
-  exitFunc = collection.getElementsByTagName("gpu:exitFunction")
-
-
-  name = ""
-
-  # init functions
-  file.write("subgraph cluster_%d{\n color=blue; label=initFunctions;penwidth=3;  \n\n" % (clusterNumber))
-  clusterNumber = clusterNumber +1
-
-  for fi in initFunc:
-    name = (fi.getElementsByTagName('gpu:name')[0]).childNodes[0].data
-    file.write("   %s [shape=box,penwidth=3];\n" %  name )
-    print ("init function: %s" % name)
-    init_flag=True
-    file.write("}\n\n")
-
-    init_name_tmp = name
-    init_temp=clusterNumber-1
-
-
-  # step functions
-  file.write("subgraph cluster_%d{\n color=blue;label=stepFunctions;penwidth=3; \n\n" % (clusterNumber))
-  clusterNumber = clusterNumber +1
-
-  for fs in stepFunc:
-    name = (fs.getElementsByTagName('gpu:name')[0]).childNodes[0].data
-    file.write("   %s [shape=box,penwidth=3];\n" %  name )
-    print ("step function: %s" % name)
-    step_flag=True
-    file.write("}\n\n")
-    step_name_tmp = name
-    step_temp=clusterNumber-1
-
-  # exit functions
-  file.write("subgraph cluster_%d{\n color=blue; label=exitFunctions;penwidth=3; \n\n" % (clusterNumber))
-  clusterNumber = clusterNumber +1
-
-  for fe in exitFunc:
-    name = (fe.getElementsByTagName('gpu:name')[0]).childNodes[0].data
-    file.write("   %s [shape=box,penwidth=3];\n" %  name )
-    print ("exit function: %s" % name)
-    exit_flag=True
-    file.write("}\n\n")
-    exit_name_tmp = name
-    exit_temp=clusterNumber-1
-
-  # fix, it should replace if not exists , get rid of if conditions
-  if (init_flag):
-    if (step_flag):
-      file.write("%s -> %s [ltail=cluster_%d,lhead=cluster_%d];\n\n"% (init_name_tmp,step_name_tmp,init_temp,step_temp))
-    elif (exit_flag):
-      file.write("%s -> %s [ltail=cluster_%d,lhead=cluster_%d];\n\n"% (init_name_tmp,exit_name_tmp,init_temp,exit_temp))
-    elif (step_flag & exit_flag):
-      file.write("%s -> %s [ltail=cluster_%d,lhead=cluster_%d];\n\n"% (step_name_tmp,exit_name_tmp,step_temp,exit_temp))
-
-
-  #################### Rank function acc to layers
+  # Populate the digraph.
+  # @todo
 
 
 
-  f_Layers = collection.getElementsByTagName("layers")[0]
-  funcSynch = f_Layers.getElementsByTagName("layer")
+  # For each function layer
+  for layerIndex, function_layer in enumerate(function_layers):
+    print("{:}:".format(layerIndex))
+    for function in function_layer:
+      print("  " + function)
 
-  file.write("{node [shape=plaintext, fontsize=16];")
-  file.write("/* the time-line graph */\n Layer")
-
-  layer=0
-  for f in funcSynch:
-    layer+=1
-    file.write("->%d" % layer)
-    file.write(";}\n\n")
-
-    layer=0  
-    for f in funcSynch:
-      layer+=1
-      fl = f.getElementsByTagName("gpu:layerFunction")
-      file.write("{rank = same ;%d;"%layer)
-      for g in fl:
-        nameFunc = (g.getElementsByTagName('name')[0]).childNodes[0].data
-        file.write("%s ;"% nameFunc)
-        file.write("}\n\n")
-
-  ####################
+  return dot
 
 
-  # Get all the agents in the collection
-  agents = collection.getElementsByTagName("gpu:xagent")
-  layers = collection.getElementsByTagName("gpu:layerFunction")
+def output_graphviz(args, graphviz_data):
+  verbose_log(args, "Outputting Graphviz Data")
 
-  # Get a list of the function execution in the correct order
-  for layer in layers:
-    nomeFuncLayer = (layer.getElementsByTagName('name')[0]).childNodes[0].data
-    funcOrder.append(nomeFuncLayer)
-    previousState.append("")
-    afterState.append("")
-    conditions.append("")
-    #print ("Function:%s\n" % (nomeFuncLayer))
+  if graphviz_data is None:
+    raise ValueError("Invalid Graphviz Data")
 
+  if args.output:
+    graphviz_data.render(args.output, view=args.render)  
+  else:
+    print("@todo - render without saving to disk?")
 
-  # Print detail of each agent.
-  for agent in agents:
-    nome = (agent.getElementsByTagName('name')[0]).childNodes[0].data
-    print ("agent: %s" % nome)
+def main():
 
-    if not primeiro:
-      file.write("}\n\n")
+  # Process command line args
+  args = parse_arguments()
+  
+  # Validate arguments
+  try:
+    validate_arguments(args)
+  except ValueError as err:
+      print(err)
+      return
 
-    primeiro = False
-    file.write("subgraph cluster_%d{\n label=\"%s\";color=blue; penwidth=3; \n\n" % (clusterNumber,nome))
-    clusterNumber = clusterNumber +1
-
-    funcs = agent.getElementsByTagName('functions')[0]
-    funs = funcs.getElementsByTagName("gpu:function")
-    for f in funs:
-      noIO =True
-      nameFunc = (f.getElementsByTagName('name')[0]).childNodes[0].data
-      if (nameFunc not in funcOrder):
-        print ('  warning : funtion %s is not in layers list.\n' % (nameFunc))
-      else: 
-        print("Function:"+nameFunc)
-        condition = getCondition(f)
-        rngT = (f.getElementsByTagName('gpu:RNG'))
-        if (rngT):
-          rng = rngT[0].childNodes[0].data
-        else:
-          rng = "false"
-          # description = f.getElementsByTagName('description')[0]
-
-        ist = (f.getElementsByTagName('currentState')[0]).childNodes[0].data
-        fst = (f.getElementsByTagName('nextState')[0]).childNodes[0].data
-
-        # print ("Funcao: %s" % nameFunc)
-
-        # Store the states of each function
-        previousState[funcOrder.index(nameFunc)] = nome+":"+ist
-        afterState[funcOrder.index(nameFunc)]=nome+":"+fst
-
-        conditions[funcOrder.index(nameFunc)]= condition
-
-        file.write("   %s [shape=box,penwidth=3];\n" %  nameFunc )
-
-        if (cycle):
-          file.write("   %s -> %s %s [penwidth=3];\n" % (ist, nameFunc, condition ))
-          file.write("   %s -> %s[penwidth=3];\n" %  (nameFunc,  fst))
-
-          outputs = f.getElementsByTagName('outputs')
-          if (outputs.length>0):
-            gpuOut = outputs[0].getElementsByTagName("gpu:output")[0]
-            menName = getNodeText(gpuOut.getElementsByTagName("messageName")[0])
-            
-            file.write("   %s -> %s [color=green4,penwidth=3];\n" %  (nameFunc,  menName))
-
-            if (menName not in mes):
-              mes.append(menName)
-
-              inputs = f.getElementsByTagName('inputs')
-              if (inputs.length>0):
-                gpuIN = inputs[0].getElementsByTagName("gpu:input")[0]
-                menName = getNodeText(gpuIN.getElementsByTagName("messageName")[0])
-
-                file.write("   %s -> %s [color=green4,penwidth=3];\n" %  ( menName, nameFunc))
-
-                if (menName not in mes):
-                  mes.append(menName)
-
-        #----------------------------------------------------------------------------------
-        #  If is not to produce cycles on the same state then the states will be renamed
-        #----------------------------------------------------------------------------------
-        if not cycle:
-          renameStates(nome)
-          last_state=0
-          for k in range(len(funcOrder)):
-            if(previousState[k].find(nome+":")==0 ):
-              file.write("   \"%s\" -> %s %s;\n" % (previousState[k][previousState[k].find(":")+1:], funcOrder[k], conditions[k] ))
-              print("   \"%s\" -> %s %s;\n" % (previousState[k][previousState[k].find(":")+1:], funcOrder[k], conditions[k] ))
-              file.write("   %s -> \"%s\";\n" %  (funcOrder[k],  afterState[k][afterState[k].find(":")+1:]))
-              print ("   %s -> \"%s\";\n" %  (funcOrder[k],  afterState[k][afterState[k].find(":")+1:]))
-              last_state=k
-              file.write("  \"%s\"-> MID [style=invis];\n" %  (afterState[last_state][afterState[last_state].find(":")+1:]))
-
-
-
-  #----------------------------------------------------------------------------------
-  #  Writes the shape of messages
-  #----------------------------------------------------------------------------------
-  file.write("}\n\n")
-
-
-  file.write(" START-> \"%s\"[style=invis] ;\n"% previousState[0][previousState[0].find(":")+1:])
-
-
-  file.write("MID -> END [style=invis];\n\n")
-
-  if (init_flag):
-    file.write("%s -> START [style=invis];\n"% (init_name_tmp))
-    file.write("{rank = same ; START ; %s;}\n"% (init_name_tmp))
-    if (step_flag):
-      file.write("{rank = same ; MID ; %s;}\n"% (step_name_tmp))
-      if (exit_flag):
-        file.write("{rank = same ; END ; %s;}\n\n"% (exit_name_tmp))
-        for m in mes:
-          file.write("   %s [shape=box][shape=diamond, label=%s, fontcolor=green4, color=green4,penwidth=3];\n" % (m,m))
-          file.write("}")
-
-
-  print(dot.source)
-  # dot.render('test-output/round-table.gv', view=True)  
-
+  # Generate the dotfile
+  try:
+    xml = load_xmlmodelfile(args)
+    graphviz_data = generate_graphviz(args, xml)
+    output_graphviz(args, graphviz_data)
+  except ValueError as err:
+      print(err)
+      return
 
 
 if __name__ == "__main__":
